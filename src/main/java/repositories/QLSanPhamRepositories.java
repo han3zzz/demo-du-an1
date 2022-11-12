c816215c81fe562394d260e9c245c7442ffca468
Hiep <hiepdsph24154@fpt.edu.vn>
2022-11-13 03:27:49 +0700

    add chuc nang ql sanPham

---
+++ src/main/java/repositories/QLSanPhamRepositories.java
@@ -0,0 +1,76 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package repositories;
+
+import domainmodels.SanPham;
+import java.util.List;
+import javax.persistence.Query;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import utilities.HibernateConfig;
+
+/**
+ *
+ * @author Admin
+ */
+public class QLSanPhamRepositories {
+    public List<SanPham> getAll(){
+        Session session = HibernateConfig.getFACTORY().openSession();
+        Query q = session.createQuery("From SanPham");
+        List<SanPham> list = q.getResultList();
+        return list;
+    }
+    public boolean add(SanPham sanPham){
+        Transaction transaction = null ;
+        try (Session s = HibernateConfig.getFACTORY().openSession()){
+            transaction = s.beginTransaction();
+            s.save(sanPham);
+            transaction.commit();
+            return true;
+            
+        } catch (Exception e) {
+            transaction.rollback();
+            return false;
+        }
+    }
+    public boolean update(SanPham sanPham){
+        Transaction transaction = null ;
+        try (Session s = HibernateConfig.getFACTORY().openSession()){
+            transaction = s.beginTransaction();
+            s.update(sanPham);
+            transaction.commit();
+            return true;
+            
+        } catch (Exception e) {
+            transaction.rollback();
+            return false;
+        }
+    }
+    public boolean delete(SanPham sanPham){
+        Transaction transaction = null ;
+        try (Session s = HibernateConfig.getFACTORY().openSession()){
+            sanPham.setTrangThai(1);
+            transaction = s.beginTransaction();
+            s.update(sanPham);
+            transaction.commit();
+            return true;
+            
+        } catch (Exception e) {
+            transaction.rollback();
+            return false;
+        }
+    }
+    public SanPham seachbyMa(String ma){
+        Session session = HibernateConfig.getFACTORY().openSession();
+        Query q = session.createQuery("From SanPham where MaSP = :ma");
+        q.setParameter("ma", ma);
+        List<SanPham> list = q.getResultList();
+        if (list.size() == 0) {
+            return null;
+        }
+        return list.get(0);
+    }
+    
+}
---
+++ src/main/java/services/QLSanPhamServices.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package services;
+
+import domainmodels.SanPham;
+import java.util.List;
+import repositories.QLSanPhamRepositories;
+import repositories.SanPhamRepositories;
+
+/**
+ *
+ * @author Admin
+ */
+public class QLSanPhamServices implements IQLSanPhamServices{
+    QLSanPhamRepositories spr;
+    
+    public QLSanPhamServices(){
+        spr = new QLSanPhamRepositories();
+    }
+
+    @Override
+    public List<SanPham> getALL() {
+        return spr.getAll();
+    }
+
+    @Override
+    public boolean add(SanPham sanPham) {
+        return spr.add(sanPham);
+    }
+
+    @Override
+    public boolean update(SanPham sanPham) {
+        return spr.update(sanPham);
+    }
+
+    @Override
+    public boolean delete(SanPham sanPham) {
+        return spr.delete(sanPham);
+    }
+
+    @Override
+    public SanPham seachbyMa(String ma) {
+        return spr.seachbyMa(ma);
+    }
+}
