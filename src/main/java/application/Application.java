b2fc7c144f69e75d5e1df2b94ea472d2dbcedb7f
hodangquan <quanhdph24157@fpt.edu.vn>
2022-11-12 20:37:29 +0700

    1

--- src/main/java/application/Application.java
+++ src/main/java/application/Application.java
@@ -21,7 +21,6 @@ public class Application {
           System.out.println("linh");
           System.out.println("tung");
           System.out.println("trung");
-          // ahnhdfhsd
           System.out.println("quan");
           
         new LoginForm().setVisible(true);
