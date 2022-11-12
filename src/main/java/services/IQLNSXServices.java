4b8f423e8f469d17d8079f42ec36f6ff776d0eeb
hodangquan <quanhdph24157@fpt.edu.vn>
2022-11-13 01:57:48 +0700

    add chuc nang ql nsx

---
+++ src/main/java/services/IQLNSXServices.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package services;
+
+import domainmodels.NSX;
+import java.util.List;
+
+/**
+ *
+ * @author hodangquan
+ */
+public interface IQLNSXServices {
+    public List<NSX> getALL();
+    public boolean add(NSX nsx);
+    public boolean update(NSX nsx);
+    public boolean delete(NSX nsx);
+    public NSX seachbyMa(String ma);
+}
